3a67898824c3bad5948ffd44dc514f146bc87bea
Your Name <you@example.com>
2024-05-23 20:46:28 -0600

    Final

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function about() {
+    return (
+        <div>
+            <h2>About Us</h2>
+            <p>this is the about page of our finnal project.</p>
+        </div>
+    );
+}
+export default about;
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Footer() {
+    return (
+        <div className="footer">
+            <p>Copyright 2024. All rights reserved.</p>
+        </div>
+    );
+}
+
+export default Footer;
