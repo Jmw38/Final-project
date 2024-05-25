4fea40730908f8094941bf152550ba540b486b54
Your Name <you@example.com>
2024-05-25 16:41:09 -0600

    final

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React from 'react'; 
-
+import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
@@ -19,6 +18,7 @@ function App() {
         <Route path="/contact" element={<Contact />} />
       <Footer />
     </Router>
+    
   );
 }
 
