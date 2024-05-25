4c5c39d4fca3876922033ce3f8b4017dfb0d56e4
Your Name <you@example.com>
2024-05-25 17:04:07 -0600

    finals

--- src/App.js
+++ src/App.js
@@ -12,12 +12,11 @@ import './App.css';
 function App() {
   return (
     <Router>
-      <Header />
-      <Navbar />
+      <switch>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-      <Footer />
+        </switch>
     </Router>
     
   );
