a4187109a83e151867adf40725f2784983c2fd10
Your Name <you@example.com>
2024-06-08 22:32:01 -0600

    final

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,6 @@ const Header = () => (
         <Nav className="mr-auto">
             <Nav.Link as={Link} to="/">Home</Nav.Link>
             <Nav.Link as={Link} to="/about">About</Nav.Link>
-            <Nav.Link as={Link} to="/todos">Todo List</Nav.Link>
             <Nav.Link as={Link} to="/add-todo">Add Todo</Nav.Link>
         </Nav>
     </Navbar>
